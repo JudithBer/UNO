3afa9b02423d3e968d4486a531ba11a63ea5736f
JudithBer <berjud2695@gmail.com>
2017-09-20 13:51:14 +0200

    Create Player.java

---
+++ src/engine/Player.java
@@ -0,0 +1,22 @@
+package engine;
+
+import gui.Card;
+
+public class Player {
+
+	Game game;
+	final int id;
+	public Player (int id){
+	this.id=id;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void placeCard(Card card){
+		
+	}
+
+	
+}
